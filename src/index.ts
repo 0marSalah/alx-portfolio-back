5788bb010904eda44fa98d5642d6d9da2bd607a3
0marSalah <omar.salah1597@gmail.com>
2023-11-01 16:59:13 +0200

    setup to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import app from './server';
 import * as dotenv from 'dotenv';
+import config from './config';
 
 dotenv.config();
 
-const port = 8888;
+const port = config.port;
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
